4231ddf2f9ee879c1f55d8e13ac837d20eb94f9a
Billy Waite <Billy.waite91@gmail.com>
2016-11-06 17:55:43 -0500

    Fully functional login and authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-	root	'static_pages#home'
+	  root	'static_pages#home'
   	get		'/help', 	to: 'static_pages#help'
   	get		'/about', 	to: 'static_pages#about'
   	get		'/contact', to: 'static_pages#contact'
   	get		'/signup', 	to: 'users#new'
-  	post	'/signup',	to: 'users#create'
+  	post	'/signup',	to: 'users#create'  #produces an error when this is not present, shouldn't need this with users resource
   	get		'/login', 	to: 'sessions#new'
   	post	'/login', 	to: 'sessions#create'
   	delete	'/logout', 	to: 'sessions#destroy'
