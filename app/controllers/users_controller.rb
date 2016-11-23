4c0a6d76c97a3bd27eb309ad308ad27e29c9908e
Billy Waite <billy.waite91@gmail.com>
2016-11-23 11:20:46 -0500

    Added forwarding functionality when user is prompted for log-in after visiting restricted page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,6 +47,7 @@ end
     # Confirms a user is logged in.
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
